9109fca5fabaaeb2970d5402bec7689fbc29be2f
shehabshalan <shehab.shalan@hotmail.com>
2022-12-09 23:14:47 +0100

    page title

---
+++ src/components/Title.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <Typography variant="h4" align="center">
+        CBR for COVID-19 Outbreaks
+      </Typography>
+
+      <Typography variant="h6" align="center">
+        A Case-Based Reasoning System
+      </Typography>
+    </div>
+  );
+};
+
+export default Title;
