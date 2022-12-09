d4704b5ddeae3eaa1e1ce931fb10d97a067d830c
shehabshalan <shehab.shalan@hotmail.com>
2022-12-09 23:39:56 +0100

    comment

--- src/components/CaseInput.jsx
+++ src/components/CaseInput.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { TextField, Grid, Box } from "@mui/material";
 
-// make a component that can be used to generate a text field should be able to be used in a form from the parent component
-//
-// props:
-//  - label: the label for the text field
-//  - value: the value of the text field
-//  - onChange: the function to call when the text field changes
-//  - error: whether or not the text field has an error
-
 const CaseInput = ({ label, value, onChange, name }) => {
   return (
     <Grid item xs={12}>
