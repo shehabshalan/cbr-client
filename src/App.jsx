790862c6deb6daa737669d8bd3201f9ba8442fed
shehabshalan <shehab.shalan@hotmail.com>
2022-12-11 19:29:49 +0100

    recommendation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import Layout from "./components/Layout";
 import CaseForm from "./components/CaseForm";
-import { Box, Grid } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import About from "./components/About";
 import Title from "./components/Title";
+import { useState } from "react";
+import Recommendation from "./components/Recommendation";
 function App() {
+  const [resources, setResources] = useState([]);
   return (
     <Layout>
       <Box sx={{ m: 2 }}>
         <Title />
       </Box>
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <CaseForm setResources={setResources} />
+        </Grid>
+
         <Grid item xs={12} md={6}>
           <About />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CaseForm />
+          <Divider />
+          {resources.length > 0 && <Recommendation data={resources} />}
         </Grid>
       </Grid>
     </Layout>
