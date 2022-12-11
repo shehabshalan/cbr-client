ebf809725bc48402a98b3bfee7e254b1a80f10df
shehabshalan <shehab.shalan@hotmail.com>
2022-12-11 19:30:00 +0100

    text align changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,8 @@ const About = () => {
   return (
     <div>
       <Box sx={{ m: 2 }}>
-        <Typography variant="h5" align="center">
-          About
-        </Typography>
-        <Typography variant="body1" align="center">
+        <Typography variant="h5">About</Typography>
+        <Typography variant="body1">
           This system is a case-based reasoning system that uses a
           knowledge-based approach to allocate resources to COVID-19 outbreaks.
           it uses a knowledge base of past outbreaks to recommend resources to
@@ -16,20 +14,16 @@ const About = () => {
         </Typography>
       </Box>
       <Box sx={{ m: 2 }}>
-        <Typography variant="h5" align="center">
-          How to Use
-        </Typography>
-        <Typography variant="body1" align="center">
+        <Typography variant="h5">How to Use</Typography>
+        <Typography variant="body1">
           To use the system, fill out the form below with the information about
           the outbreak. The system will then use the information to recommend
           resources to allocate.
         </Typography>
       </Box>
       <Box sx={{ m: 2 }}>
-        <Typography variant="h5" align="center">
-          About the System
-        </Typography>
-        <Typography variant="body1" align="center">
+        <Typography variant="h5">About the System</Typography>
+        <Typography variant="body1">
           This system was built using React and Material-UI. The system uses
           Flask as a backend to run the case-based reasoning system. This is
           still a work in progress, so please check back later for updates.
