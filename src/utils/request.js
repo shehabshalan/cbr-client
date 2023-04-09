ed76fe5e006d21ddd8d90032b8df9fd0b6b5d872
shehabshalan <shehab.shalan@hotmail.com>
2023-04-10 00:04:41 +0200

    chore: update api endpoint

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const request = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://covid-19-recommendation.herokuapp.com"
+      ? "https://cbr-api.onrender.com"
       : "http://localhost:8000",
 });
