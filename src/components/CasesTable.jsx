15391c4c51830da33894fd827b8a3db56ce1518b
shehabshalan <shehab.shalan@hotmail.com>
2023-04-10 00:04:58 +0200

    feat: case library table

---
+++ src/components/CasesTable.jsx
@@ -0,0 +1,143 @@
+import { useGetCases } from "../hooks/useGetCases";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Tooltip,
+  Button,
+} from "@mui/material";
+
+const CasesTable = () => {
+  const { data: cases, isLoading, isError } = useGetCases();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  if (cases.length === 0) {
+    return <div>No cases found</div>;
+  }
+
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{
+        maxWidth: 1200,
+      }}
+    >
+      <Table
+        aria-label="cases table"
+        sx={{
+          width: "max-content",
+        }}
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell>Case ID</TableCell>
+            <TableCell align="right">Start Date</TableCell>
+            <TableCell align="right">End Date</TableCell>
+            <TableCell align="right">City</TableCell>
+            <TableCell align="right">Population</TableCell>
+            <TableCell align="right">Population Density</TableCell>
+            <TableCell align="right">Age Distribution</TableCell>
+            <TableCell align="right">Problem Description</TableCell>
+            <TableCell align="right">Number of Active Cases at Start</TableCell>
+            <TableCell align="right">Number of Active Cases at End</TableCell>
+            <TableCell align="right">
+              Number of ICU Active Cases at Start
+            </TableCell>
+            <TableCell align="right">
+              Number of ICU Active Cases at End
+            </TableCell>
+            <TableCell align="right">Number of Deaths at Start</TableCell>
+            <TableCell align="right">Number of Deaths at End</TableCell>
+            <TableCell align="right">Vaccinated Population</TableCell>
+            <TableCell align="right">Infection Rate</TableCell>
+            <TableCell align="right">Mortality Rate</TableCell>
+            <TableCell align="right">Solution Description</TableCell>
+            <TableCell align="right">Lockdown Policy</TableCell>
+            <TableCell align="right">Mask Policy</TableCell>
+            <TableCell align="right">Vaccination Policy</TableCell>
+            <TableCell align="right">Solution Effectiveness</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cases.map((row) => (
+            <TableRow
+              key={row.case_id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.start_date}</TableCell>
+              <TableCell align="right">{row.end_date}</TableCell>
+              <TableCell align="right">{row.city}</TableCell>
+              <TableCell align="right">{row.problem_population}</TableCell>
+              <TableCell align="right">
+                {row.problem_population_density}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_age_distribution}
+              </TableCell>
+              <TableCell align="right">
+                <Tooltip title={row.problem_description}>
+                  <Button>View</Button>
+                </Tooltip>
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_start_number_of_active_cases}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_end_number_of_active_cases}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_start_number_of_icu_active_cases}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_end_number_of_icu_active_cases}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_start_number_of_deaths}
+              </TableCell>
+              <TableCell align="right">
+                {row.problem_end_number_of_deaths}
+              </TableCell>
+
+              <TableCell align="right">
+                {row.problem_vaccinated_population}
+              </TableCell>
+              <TableCell align="right">{row.problem_infection_rate}</TableCell>
+              <TableCell align="right">{row.problem_mortality_rate}</TableCell>
+              <TableCell align="right">
+                <Tooltip title={row.solution_description}>
+                  <Button>View</Button>
+                </Tooltip>
+              </TableCell>
+              <TableCell align="right">
+                {row.solution_lockdown_policy_level}
+              </TableCell>
+              <TableCell align="right">
+                {row.solution_mask_policy_level}
+              </TableCell>
+              <TableCell align="right">
+                {row.solution_vaccine_policy_level}
+              </TableCell>
+              <TableCell align="right">{row.solution_effectiveness}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default CasesTable;
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,7 +1,35 @@
+import { Alert, AlertTitle, Grid, Typography } from "@mui/material";
 import React from "react";
+import CasesTable from "../components/CasesTable";
 
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      sx={{
+        maxWidth: 1200,
+        margin: "auto",
+      }}
+    >
+      <Grid item xs={12}>
+        <Alert severity="info">
+          <AlertTitle>Case Library</AlertTitle>
+
+          <Typography variant="body1" align="justify">
+            The case library is the accumulated knowledge which the system uses
+            to make recommendations. The table below shows the cases in the
+            library. The cases are sorted by the date they were added to the
+            library.
+          </Typography>
+        </Alert>
+      </Grid>
+      <Grid item xs={12}>
+        <CasesTable />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Library;
