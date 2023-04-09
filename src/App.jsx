e79a1b57d9226bbae89bcf9d8b44e708b0fcd499
shehabshalan <shehab.shalan@hotmail.com>
2023-04-09 16:18:56 +0200

    feat: mui and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Recommender from "./pages/Recommender";
+import Builder from "./pages/Builder";
+import Library from "./pages/Library";
+import About from "./Pages/About";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import CaseForm from "./components/CaseForm";
-import { Box, Divider, Grid, Typography } from "@mui/material";
-import About from "./components/About";
-import Title from "./components/Title";
-import { useState } from "react";
-import Recommendation from "./components/Recommendation";
+import "./App.css";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000 * 60, // 5 minutes
+      cacheTime: Infinity, // do not delete stale data
+    },
+  },
+});
 function App() {
-  const [resources, setResources] = useState([]);
   return (
-    <Layout>
-      <Box sx={{ m: 2 }}>
-        <Title />
-      </Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <CaseForm setResources={setResources} />
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <About />
-          <Divider />
-          {resources.length > 0 && <Recommendation data={resources} />}
-        </Grid>
-      </Grid>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Recommender />} />
+          <Route path="/builder" element={<Builder />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/Recommender.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Recommender = () => {
+  return <div>Recommender </div>;
+};
+
+export default Recommender;
