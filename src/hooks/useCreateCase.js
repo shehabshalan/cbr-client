1e8ce18809186bc9b29e1d97205d12ffd7a590b0
shehabshalan <shehab.shalan@hotmail.com>
2023-04-14 00:53:47 +0200

    chore: invalidate cases query upon creating new case

--- src/hooks/useCreateCase.js
+++ src/hooks/useCreateCase.js
@@ -1,8 +1,16 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { request } from "../utils/request";
 
 export const useCreateCase = () => {
+  const queryClient = useQueryClient();
   return useMutation(
-    async (payload) => await request.post("/cases/create", payload)
+    async (payload) => {
+      await request.post("/cases/create", payload);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("cases");
+      },
+    }
   );
 };
